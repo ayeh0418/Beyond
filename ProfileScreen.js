225fa7c88c57e71e6a93b8ec7d3d14a91c31d5b7
Angela Emerick <eignastar@gmail.com>
2021-02-21 09:28:09 -0800

    done kind of

--- ProfileScreen.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Text } from 'react-native';
-
-export default function ProfileScreen ({ navigation, route }) {
-    return <Text>This is {route.params.name}'s profile</Text>;
-  };
